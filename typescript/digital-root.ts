eedb664eb5b622c65e3e86100deb8bdf19ddec8d
tomasz-jankowski <]GJ+]Qh],[&:E4qQ>
2022-09-20 13:31:09 +0200

    Day 0

---
+++ typescript/digital-root.ts
@@ -0,0 +1,23 @@
+/*
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+
+export const digitalRoot = (n: number): number => {
+  const str = n.toString();
+  if (str.length === 1) return n;
+
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += +str.charAt(i);
+  }
+
+  return digitalRoot(sum);
+};
